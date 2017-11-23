7818694b2c8eb984e12916bea3743e3d9bf5be70
Claudiu Soroiu <claudiu.soroiu@tora.com>
2017-11-23 12:18:12 +0200

    bump version to 2.8.2-4 and added missing methods/classes

---
+++ log4j-1.2-api/src/main/java/org/apache/log4j/spi/LocationInfo.java
@@ -0,0 +1,13 @@
+package org.apache.log4j.spi;
+
+/**
+ * Date: 22.11.2017
+ * Time: 16:58
+ *
+ * @author <a href="mailto:gyuri.majercsik@tora.com">Gyuri Majercsik</a>
+ */
+public class LocationInfo {
+
+	public LocationInfo(Throwable throwable, String fqnOfCategoryClass) {
+	}
+}
--- log4j-1.2-api/src/main/java/org/apache/log4j/spi/LoggingEvent.java
+++ log4j-1.2-api/src/main/java/org/apache/log4j/spi/LoggingEvent.java
@@ -1,35 +1,52 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache license, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the license for the specific language governing permissions and
- * limitations under the license.
- */
 package org.apache.log4j.spi;
 
 import org.apache.log4j.Category;
-import org.apache.log4j.Priority;
+import org.apache.log4j.Level;
+
+import java.util.Map;
 
 /**
- *  No-op version of Log4j 1.2 LoggingEvent.
+ * No-op version of Log4j 1.2 LoggingEvent.
  */
-public class LoggingEvent {
+public class LoggingEvent implements java.io.Serializable {
 
-	public LoggingEvent() {
-		// nothing to do
+	public static final String[] EMPTY_STRING_ARRAY = new String[0];
+	private Category logger;
+	private final Level level;
+	public String fqnOfCategoryClass;
+	public long timeStamp;
+
+	public LoggingEvent(String fqnOfCategoryClass,
+						Category logger,
+						long timeStamp,
+						Level level,
+						Object message,
+						String threadName,
+						ThrowableInformation throwable,
+						String ndc,
+						LocationInfo info,
+						Map properties) {
+		this.logger = logger;
+		this.level = level;
 	}
 
-	public LoggingEvent(String fqnOfCategoryClass, Category logger,
-						Priority level, Object message, Throwable throwable) {
-		// nothing to do
+	public String getRenderedMessage() {
+		return "";
+	}
+
+	public Category getLogger() {
+		return logger;
+	}
+
+	public String[] getThrowableStrRep() {
+		return EMPTY_STRING_ARRAY;
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+	public String getThreadName() {
+		return Thread.currentThread().getName();
 	}
 }
